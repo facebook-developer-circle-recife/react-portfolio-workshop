1d19a38310304e3670ae1aef829f55e2f5a92dcc
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2019-09-21 17:05:56 -0300

    DE

---
+++ precache-manifest.92e83bc2d26dffe252f4be65af4a9f13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7682a5142a1f188072dcba4b40a8ca81",
+    "url": "/react-portfolio-workshop/index.html"
+  },
+  {
+    "revision": "7c8221cbee30a66cd5b1",
+    "url": "/react-portfolio-workshop/static/css/main.3bb9bdd3.chunk.css"
+  },
+  {
+    "revision": "61c8c26cbecc4d327b28",
+    "url": "/react-portfolio-workshop/static/js/2.8d6ee4ec.chunk.js"
+  },
+  {
+    "revision": "7c8221cbee30a66cd5b1",
+    "url": "/react-portfolio-workshop/static/js/main.3f90e1bb.chunk.js"
+  },
+  {
+    "revision": "7f60ed631bcf2fa3ab37",
+    "url": "/react-portfolio-workshop/static/js/runtime-main.21db6aff.js"
+  },
+  {
+    "revision": "a170675dae7e7df9a1c6c4002d1abd27",
+    "url": "/react-portfolio-workshop/static/media/eu.a170675d.png"
+  }
+]);
